1b85fd15a67550808d443ec583dff0e84cac0d64
fazzy305 <62588835+fazzy305@users.noreply.github.com>
2025-05-10 11:40:06 +0800

    Update zipalign_sign.sh
    test ndk path

--- zipalign_sign.sh
+++ zipalign_sign.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# 调试：输出 Android SDK 路径
+echo "ANDROID_HOME: $ANDROID_HOME"
+# 调试：检查 zipalign 是否存在
+ls -l $ANDROID_HOME/build-tools/32.0.0/zipalign
+
 PATH=$PATH:$ANDROID_HOME/build-tools/32.0.0/
 for f in build/*.apk; do
     mv $f ${f%.apk}.apk.unsigned
