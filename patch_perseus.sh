6200516d1536e38778cb70644ab2ccd9854f3f2d
fazzy305 <62588835+fazzy305@users.noreply.github.com>
2025-09-03 20:19:30 +0800

    Update patch_perseus.sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -49,7 +49,6 @@ if [ ! -f "${bundle_id}.apk" ]; then
             ;;
         "com.bilibili.blhx.m4399")
             if wget "https://pan.gfwl.top/f/y64hj/blhx.apk" -O "${bundle_id}.apk" -q; then
-            if wget "https://pan.gfwl.top/f/AaX5Fr/blhx.apk" -O "${bundle_id}.apk" -q; then
                 download_success=true
             fi
             ;;
